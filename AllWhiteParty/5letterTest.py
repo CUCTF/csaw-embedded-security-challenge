0da55d63ec7c86b59a0105c70c05149c41af5b9a
IzzyFG <izardfg@gmail.com>
2023-10-16 22:59:22 -0400

    narrowed down 2 letters

--- AllWhiteParty/5letterTest.py
+++ AllWhiteParty/5letterTest.py
@@ -4,10 +4,18 @@ import string
 import time
 
 def generate_combinations(length=5):
-    letters = string.ascii_letters + string.digits
-    return itertools.product(letters, repeat=length)
+    letters = string.ascii_letters #+ string.digits
+    four = letters[7:8]
+    # AF_upper = ['A','B','C','D','E','F']
+    five = ['A']
+    output = itertools.product(letters, letters, letters,four,five)
+    # output = itertools.combinations_with_replacement(AF_upper,5)
+
+    # output = itertools.product(letters, repeat=length)
+    return output
 
 def main():
+
     # LINUX:
     # ser = serial.Serial('/dev/ttyUSB0', baudrate=115200 , timeout=1)
 
@@ -20,7 +28,7 @@ def main():
     ser.open()
 
     ## get beginning lines
-    time.sleep(1)
+    time.sleep(2)
     response = ser.readline().decode('utf-8')
     while response:
         print(f"{response}")
@@ -33,7 +41,7 @@ def main():
 
         ser.write(data.encode('utf-8'))
 
-        time.sleep(3)
+        time.sleep(5)
 
         response = ser.readline().decode('utf-8').strip()
         if response:
