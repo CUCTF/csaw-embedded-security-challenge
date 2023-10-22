3189772db4cf35eae0bc2887363c5c72a15c7330
IzzyFG <izardfg@gmail.com>
2023-10-22 18:17:22 -0400

    decoded to b64

--- czNxdTNuYzM/decoder.py
+++ czNxdTNuYzM/decoder.py
@@ -1,4 +1,5 @@
-import codecs
+import base64
+from binascii import b2a_base64
 import os
 
 # Gets each digit series(w/o "." or "_")inside of phrase and puts it in a list
@@ -9,15 +10,39 @@ def get_data(filename):
 			lines.append(line.rstrip()[:-1].replace("_",""))
 	return lines
 
+def generate_hex(toHex):
+	hexed = []
+	for i in toHex:
+		temp = hex(int(i))
+		if (len(temp)%2 == 0):
+			temp = temp.replace("0x","")
+		else:
+			temp = temp.replace("0x","0")
+		hexed.append (bytes.fromhex(temp))
+	return hexed
+	
+def generateB64(toB64):
+	b64digits = []
+	for i in toB64:
+		b64digits.append ( b2a_base64(i).decode())
+	return b64digits
 
-digits = get_data("csaw-embedded-security-challenge/czNxdTNuYzM/digits.txt")
-# print (digits)
+def writeOut(data, filename):
+	f = open(filename, "w")
+	for x in data:	
+		f.write(x)
+	f.close()
 
-# print (os.getcwd())
-# decode_hex = codecs.getdecoder("hex_codec")
+def main():
+	digits = get_data("csaw-embedded-security-challenge/czNxdTNuYzM/digits.txt")
+	hexed = generate_hex(digits)
+	print(hexed)
 
-# # for an array
-# msgs = [decode_hex(msg)[0] for msg in msgs]
+	b64digits = generateB64(hexed)
+	print(f"B64: {b64digits}")
 
-# # for a string
-# string = decode_hex(string)[0]
+	writeOut(b64digits, "csaw-embedded-security-challenge/czNxdTNuYzM/base64_translated.txt")
+	
+
+if __name__ == '__main__':
+	main()
